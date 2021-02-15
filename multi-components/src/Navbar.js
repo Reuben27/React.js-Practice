44c6d789440dfffb6a8cafca1ceb51e53e8fdbbc
Reuben27 <reuben.sd@iitgn.ac.in>
2021-02-15 13:19:36 +0530

    Added Home and Navbar

---
+++ multi-components/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className = "navbar">
+            <h1> Just a Navbar </h1>
+            <div className = "links">
+                <a href = "/"> Home </a>
+                <a href = "reactjs.org"> React.js </a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
