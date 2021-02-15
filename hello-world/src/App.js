7bec9eb6671ca53aaf09724c530b10ddc2c88a8d
Reuben27 <reuben.sd@iitgn.ac.in>
2021-02-15 12:55:38 +0530

    Components and Templates

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+//JSX - HTML like templates i.e. syntax identical to HTML
+//In the background a transpiler called Babel converts the JSX temmplate into HTML and renders it to the DOM.
+//Function name should start with a capital letter.
+//React websites are made up of multiple components like this.
+//A component is generally a function that returns a JSX Template
 function App() {
   return (
     <div className="App">
@@ -23,3 +28,4 @@ function App() {
 }
 
 export default App;
+//To export the component function so that it can be used by other files.
