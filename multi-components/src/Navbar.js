c1e40279e9e61f9329c7ff281d94fd7802c976eb
Reuben27 <reuben.sd@iitgn.ac.in>
2021-02-16 12:03:52 +0530

    Using statehook

--- multi-components/src/Navbar.js
+++ multi-components/src/Navbar.js
@@ -4,10 +4,16 @@ const Navbar = () => {
             <h1> Just a Navbar </h1>
             <div className = "links">
                 <a href = "/"> Home </a>
-                <a href = "reactjs.org"> React.js </a>
+                <a href = "reactjs.org" style = {{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px",
+                }}> React.js </a>
             </div>
         </nav>
     );
 }
- 
+
+//background-color does not work in JSX as - is the minus sign so Camel case is used.
+//{} = Dynamic Value
 export default Navbar;
