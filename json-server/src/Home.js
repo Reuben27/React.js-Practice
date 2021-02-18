3eb097d51a76453f7051af9efd0186d9e56e42a2
Reuben27 <reuben.sd@iitgn.ac.in>
2021-02-18 22:14:19 +0530

    error handling

--- json-server/src/Home.js
+++ json-server/src/Home.js
@@ -9,20 +9,34 @@ import BlogList from "./BlogList";
 // /blogs/{id} = Delete = Delete a blog
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if(!res.ok){
+          throw Error('The data could not be fetched');
+        }
         return res.json();
       })
       .then((data) => {
         console.log(data);
         setBlogs(data); //State Hook wont cause infinte loops here due to the empty dependency [] in the useEffect.
+        setIsPending(false);
+        setError(null);
       })
+      .catch((err) => {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
       {/*blogs is null intially so the below statement only renders when blogs data has been fetched!*/}
       {blogs && <BlogList blogs = {blogs} title = "All Blogs" />}
     </div>
