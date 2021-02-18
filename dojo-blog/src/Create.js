ff05e6ff50eeafb8e44e4f7529d7bd385b9cf79b
Reuben27 <reuben.sd@iitgn.ac.in>
2021-02-18 23:58:31 +0530

    Add new blog page created

---
+++ dojo-blog/src/Create.js
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+        fetch('http://localhost:8000/blogs');
+    }
+    
+    return ( 
+        <div className="create">
+            <h2>Add a new blog.</h2>
+            <form onSubmit = {handleSubmit}>
+                <label>Blog Title: </label>
+                <input type = "text" required value = {title} 
+                        onChange = { (e) =>{
+                            setTitle(e.target.value);
+                        }}></input>
+
+                <label>Blog Body: </label>
+                <textarea required value = {body} 
+                        onChange = { (e) =>{
+                            setBody(e.target.value);
+                        }}></textarea>
+
+                <label>Blog Author: </label>
+                <select value = {author} onChange = {
+                        (e) => {
+                            setAuthor(e.target.value);
+                        }}> 
+                    <option value = "mario">Mario</option>
+                    <option value = "yoshi">Yoshi</option>
+                </select>
+
+                <button>Add Blog</button>
+
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
+        </div>
+    );
+}
+ 
+export default Create;
---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,24 @@
+//import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+//Json server considers the top level property as a resource. It creates end points to interact with them.
+//npx json-server --watch data/db.json --port 8000
+// /blogs = Get = Fetch all blogs
+// /blogs/{id} = Get = Fetch a single blog
+// /blogs = Post = Add a new blog
+// /blogs/{id} = Delete = Delete a blog
+const Home = () => {
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
+      {/*blogs is null intially so the below statement only renders when blogs data has been fetched!*/}
+      {blogs && <BlogList blogs = {blogs} title = "All Blogs" />}
+    </div>
+  );
+}
+ 
+export default Home;
